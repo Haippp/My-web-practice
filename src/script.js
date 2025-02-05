1e5ca1aeaa12b0643f38901261fea520292036e8
Haippp <haiperdesain@gmail.com>
2025-02-05 18:05:53 +0800

    web guess word Finnaly Done!

--- src/script.js
+++ src/script.js
@@ -1,69 +1,67 @@
 const inputs = document.querySelector(".input"),
   resetButton = document.querySelector(".reset-btn"),
-  enterButton = document.querySelector(".enter-btn"),
-  hints = document.querySelector(".hint span");
+  typingInput = document.querySelector(".typing-input"),
+  hints = document.querySelector(".hint span"),
+  guessLeft = document.querySelector(".guess-left  span"),
+  wrongLetter = document.querySelector(".wrong-letter span");
 
-let word; // Menyimpan kata yang dipilih
-let incorrect = []; // Menyimpan huruf yang salah
-let remainingGuesses = 10; // Menyimpan jumlah tebakan tersisa
+let word, maxGuess, incorrects = [], corrects = [];
 
 function randomWord() {
   let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
   word = ranObj.word; // Menyimpan kata yang dipilih
+  maxGuess = 8, incorrects = [], corrects = [];
   console.log(word);
 
   hints.innerText = ranObj.hint;
+  wrongLetter.innerHTML = incorrects;
+  guessLeft.innerText = maxGuess;
 
   let html = "";
   for (let i = 0; i < word.length; i++) {
-    html += `<input class="input${i}" type="text" maxlength="1" autofocus/>`;
+    html += `<input type="text" disabled/>`;
   }
   inputs.innerHTML = html;
 }
 
+randomWord();
+
 function initGame(e) {
   let key = e.target.value;
-  if (key.match(/^[A-Za-z]+$/)) {
+  if (key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key}`) && !corrects.includes(key)) {
     console.log(key);
-    if (word.includes(key)) {
+    if (word.includes(key)) { //if user letter found in the word
       for (let i = 0; i < word.length; i++) {
-        if (word[i] === key) {
-          inputs.querySelector("input")[i].value = key;
+        //showing matched letter in the input value
+        if (word[i] ===  key) {
+          corrects.push(key);
+          inputs.querySelectorAll("input")[i].value = key;
         }
       }
     } else {
-      incorrect.push(key);
-      remainingGuesses--; // Mengurangi tebakan tersisa
+      maxGuess--;
+      incorrects.push(` ${key}`);
     }
+    guessLeft.innerText = maxGuess;
+    wrongLetter.innerHTML = incorrects;
   }
+  typingInput.value = "";
+
+  setTimeout(() => {
+    if(corrects.length === word.length){
+      alert("Selamat kamu menang!");
+      randomWord();
+    }
+    else if(maxGuess < 1){
+      alert("Game Over! Jawaban sebenarnya adalah :");
+      for(let i = 0 ; i < word.length ; i++){
+        inputs.querySelectorAll("input")[i].value = word[i];
+      }
+    }
+  })
 }
 
-randomWord();
-
 resetButton.addEventListener("click", randomWord);
-enterButton.addEventListener("click", () => {
-  const inputValues = Array.from(inputs.querySelectorAll("input")).map(input => input.value);
-  if (inputValues.includes('')) {
-    alert("Masukkan teks terlebih dahulu");
-  } else {
-    inputValues.forEach((value, index) => {
-      if (value === word[index]) {
-        inputs.querySelector(`.input${index}`).value = value; // Menjaga huruf yang benar tetap di input
-      }
-    });
-  }
-});
-
-document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("keyup", function () {
-    if (this.value.length === this.maxLength) {
-      let nextInput = this.nextElementSibling;
-      while (nextInput && nextInput.tagName !== "INPUT") {
-        nextInput = nextInput.nextElementSibling;
-      }
-      if (nextInput) {
-        nextInput.focus();
-      }
-    }
-  });
-});
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
